0506c912829c8314d852ba51187ee0e93b426cca
Dagmawi Teferi <dagiteferi2011@gmail.com>
2025-01-07 10:32:39 -0500

    `Update app.py: add import os, modify app.run() call`

--- App/app.py
+++ App/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -62,4 +63,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=os.environ.get('PORT', 3000))
